0a35e2a37c5197fb625e71a239d24385882c92a8
LUCAS KRETSCHMAR <85629797+Kretschmar-git@users.noreply.github.com>
2025-10-30 20:56:10 -0300

    Update PHPMailer library paths in processa_envio.php

--- app_send_mail/processa_envio.php
+++ app_send_mail/processa_envio.php
@@ -1,7 +1,8 @@
 <?php   
     // --- Bibliotecas do PHP Mailer --- // 
-    require '../../app_send_mail/bibliotecas/PHPMailer/src/Exception.php'; // necessario 
-    require '../../app_send_mail/bibliotecas/PHPMailer/src/PHPMailer.php'; // necessario 
-    require '../../app_send_mail/bibliotecas/PHPMailer/src/SMTP.php';      // necessario 
+    require '../../app_send_mail_P/bibliotecas/PHPMailer/src/Exception.php'; // necessario 
+    require '../../app_send_mail_P/bibliotecas/PHPMailer/src/PHPMailer.php'; // necessario 
+    require '../../app_send_mail_P/bibliotecas/PHPMailer/src/SMTP.php';      // necessario 
 
-    require_once('../../app_send_mail/processa_envio.php');
+
+    require_once('../../app_send_mail_P/processa_envio.php');
